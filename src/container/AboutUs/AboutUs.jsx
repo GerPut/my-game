27a62603a75eb8b2d367f1d64a1ebbb2404cb208
Gerhard <gerhard.putter@yahoo.com>
2023-09-24 15:58:15 +0200

    Work on about section.

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -22,17 +22,20 @@ const AboutUs = () => {
 
                 <div id="about-us-section">
 
-                    <div className="about-us-image">
+                    {/* <div className="about-us-image">
 
-                        {/* <img src={numbers} width={808} height={458} alt="Lobby Image" /> */}
+                        <img src={numbers} width={808} height={458} alt="Lobby Image" />
 
-                    </div>
+                    </div> */}
 
                     <div className="about-us-info">
 
-                        <h2>We are Digital Upgrade</h2>
+                        <h2>Predictions made easy</h2>
+                        <br />
 
-                        <p>Digital Upgrade, located in Evansville, IN, makes it easy for businesses to create and manage their digital presence and logistics, meaning we can make your business more efficient and profitable! We believe in our community and want to help our local business economy grow and expand. We believe in our clients, and want to assist in their digital success. The D-UP team grades ourselves on outcomes, not on sales.</p>
+                        <p>Fight Furnace is a tool for assessing the likelihood of an MMA athlete emerging victorious in a bout. Our calculator is equipped with a comprehensive set of predefined parameters, encompassing crucial factors to consider when evaluating the statistical profiles of two competing athletes.<br /><br />
+                            While nothing may be the ultimate solution for fight predictions, Fight Furnace eliminates human emotions from the equation by delivering results based on the provided inputs.To kickstart your journey and delve deeper into our tool, we invite you to explore our informative 'How-To' guide.<br /><br /> Let's get started and uncover more about the power of Fight Furnace!
+                        </p>
 
                         {/* <a href="#" title="About Us Button">ABOUT US</a> */}
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <>
             <div className="app__header app__wrapper section__padding" id="home">
-                <h1 className='app__header-h1'>BETTER PICKS</h1>
-                <h3 className="app__subtext">with the best MMA stats calculator</h3>
+                <h1 className='app__header-h1'>YOUR OWN ODDS</h1>
+                <h3 className="app__subtext">with the best MMA calculator</h3>
                 <div className="header__button-box">
                     <a className="button" href="#" >
                         <span></span>

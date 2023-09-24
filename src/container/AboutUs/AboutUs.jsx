9c9b40b5f66705736bea36326cd55e5fdf623213
Gerhard <gerhard.putter@yahoo.com>
2023-09-24 16:03:44 +0200

    Update header.

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -34,7 +34,7 @@ const AboutUs = () => {
                         <br />
 
                         <p>Fight Furnace is a tool for assessing the likelihood of an MMA athlete emerging victorious in a bout. Our calculator is equipped with a comprehensive set of predefined parameters, encompassing crucial factors to consider when evaluating the statistical profiles of two competing athletes.<br /><br />
-                            While nothing may be the ultimate solution for fight predictions, Fight Furnace eliminates human emotions from the equation by delivering results based on the provided inputs.To kickstart your journey and delve deeper into our tool, we invite you to explore our informative 'How-To' guide.<br /><br /> Let's get started and uncover more about the power of Fight Furnace!
+                            To kickstart your journey and delve deeper into our tool, we invite you to explore our informative 'How-To' guide. Let's get started and uncover more about the power of Fight Furnace!
                         </p>
 
                         {/* <a href="#" title="About Us Button">ABOUT US</a> */}

de4f7838230f0b6e378026c4a040311d5ea43fa9
Gerhard <gerhard.putter@yahoo.com>
2023-10-06 15:51:00 +0200

    Add Other component with toggles.

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Other() {
     return (
-        <div>Other</div>
+        <>
+            <div>Other</div>
+            <div className="d-flex justify-content-center align-items-center">
+                <div class="row align-items-center justify-content-evenly">
+                    <label for="labelDemo">Weak Chin</label>
+                    <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                </div>
+                <div class="row align-items-center justify-content-evenly">
+                    <label for="labelDemo" >Bad Cardio</label>
+                    <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                </div>
+                <div class="row align-items-center justify-content-evenly">
+                    <label for="labelDemo" >Debut</label>
+                    <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                </div>
+            </div>
+        </>
     )
 }
 

e88b1d7dfb94f15e2e7dac114af02366b25677f1
Gerhard <gerhard.putter@yahoo.com>
2023-10-06 15:51:50 +0200

    Add extra toggles for criteria.

--- src/components/FFCalculator/FFCalculator.jsx
+++ src/components/FFCalculator/FFCalculator.jsx
@@ -17,6 +17,66 @@ const FFCalculator = () => {
         <>
             <h3 className="text-center app__navbar-logo ">FIGHT FURNACE</h3>
             <div className="col-12">
+                <div>
+                    Select Your Criteria
+                </div>
+                <div className="d-flex row">
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Age</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Experience</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Win/Lose Ratio</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Streak</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Reach</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Last Active</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                    </div>
+
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Stike Accuracy</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Stike Defense</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Takedown Accuracy</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Takedown Defense</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Previous Opponents W/L</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                        <div class="row align-items-center justify-content-evenly">
+                            <label for="labelDemo">Other</label>
+                            <input id="labelDemo" type="checkbox" data-toggle="toggle" data-size="xs" />
+                        </div>
+                    </div>
+                </div>
+
+
                 <div className="d-flex row justify-content-center align-items-center">
                     <div class="col-md-3">
                         <label for="nameA" class="form-label">NAME A</label>
@@ -39,7 +99,6 @@ const FFCalculator = () => {
                 <TakeDownDef />
                 <PreviousOppWL />
                 <Other />
-
             </div>
 
 
